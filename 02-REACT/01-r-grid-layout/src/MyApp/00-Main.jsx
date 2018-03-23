b66cb16ce2d1e2615fe8c64f0a80ec4640a43e99
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-23 15:07:57 -0600

    update links

--- 02-REACT/01-r-grid-layout/src/MyApp/00-Main.jsx
+++ 02-REACT/01-r-grid-layout/src/MyApp/00-Main.jsx
@@ -3,11 +3,20 @@ import '../../node_modules/react-grid-layout/css/styles.css'
 import '../../node_modules/react-resizable/css/styles.css'
 var ReactGridLayout = require('react-grid-layout');
 
+// Information:
+// https://www.npmjs.com/package/react-grid-layout#usage
+// https://react.rocks/example/react-grid-layout
+// https://strml.github.io/react-grid-layout/examples/1-basic.html
+
+// Examples:
+// https://react.rocks/example/formbuilder
+// https://react.rocks/example/react-trello-board
+
 
 export default class extends React.Component{
     render(){
 
-        let layout = [
+        let layout1 = [
             {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
             {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
             {i: 'c', x: 4, y: 0, w: 1, h: 2}
@@ -21,7 +30,7 @@ export default class extends React.Component{
             <div>
                 <h1>Hello from MAIN component - 2</h1>
 
-                <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+                <ReactGridLayout className="layout" layout={layout1} cols={12} rowHeight={30} width={1200}>
                     <div style={aStyle} key="a">Camaro</div>
                     <div style={bStyle} key="b">Leones</div>
                     <div style={cStyle} key="c">Isla</div>
