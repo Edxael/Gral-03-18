44e194a03faa2181abb47b6a6551a8159302837b
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-23 15:45:03 -0600

    add notes

--- 02-REACT/01-r-grid-layout/src/MyApp/00-Main.jsx
+++ 02-REACT/01-r-grid-layout/src/MyApp/00-Main.jsx
@@ -3,15 +3,6 @@ import '../../node_modules/react-grid-layout/css/styles.css'
 import '../../node_modules/react-resizable/css/styles.css'
 var ReactGridLayout = require('react-grid-layout');
 
-// Information:
-// https://www.npmjs.com/package/react-grid-layout#usage
-// https://react.rocks/example/react-grid-layout
-// https://strml.github.io/react-grid-layout/examples/1-basic.html
-
-// Examples:
-// https://react.rocks/example/formbuilder
-// https://react.rocks/example/react-trello-board
-
 
 export default class extends React.Component{
     render(){
@@ -22,6 +13,7 @@ export default class extends React.Component{
             {i: 'c', x: 4, y: 0, w: 1, h: 2}
           ]
         
+        const gCont1 = { border: "1px solid black", padding: "6px", backgroundColor: "yellow" }
         const aStyle = { backgroundColor: "blue", border: "1px solid black", padding: "3px", marging: "2px" }
         const bStyle = { backgroundColor: "salmon", border: "1px solid black", padding: "3px", marging: "2px" }
         const cStyle = { backgroundColor: "aquamarine", border: "1px solid black", padding: "3px", marging: "2px" }
@@ -30,7 +22,7 @@ export default class extends React.Component{
             <div>
                 <h1>Hello from MAIN component - 2</h1>
 
-                <ReactGridLayout className="layout" layout={layout1} cols={12} rowHeight={30} width={1200}>
+                <ReactGridLayout style={gCont1} className="layout" layout={layout1} cols={12} rowHeight={30} width={1200}>
                     <div style={aStyle} key="a">Camaro</div>
                     <div style={bStyle} key="b">Leones</div>
                     <div style={cStyle} key="c">Isla</div>
---
+++ 02-REACT/01-r-grid-layout/zinfo.md
@@ -0,0 +1,15 @@
+
+# Information:
+https://www.npmjs.com/package/react-grid-layout#usage
+https://react.rocks/example/react-grid-layout
+https://strml.github.io/react-grid-layout/examples/1-basic.html
+https://github.com/STRML/react-grid-layout
+
+
+## Examples:
+https://react.rocks/example/formbuilder
+https://react.rocks/example/react-trello-board
+
+
+## FOR THE CSS
+http://tachyons.io/
