e7215ab2d9abbed4363b68f1d27b61936aa6277a
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-24 11:10:39 -0600

    refactor code

--- 04-API/04-Weather/01-Weather-Node/server.js
+++ 04-API/04-Weather/01-Weather-Node/server.js
@@ -6,9 +6,9 @@ const app = Express()
 const db = require('mongoose')
 const axios = require('axios')
 const router = Express.Router()
-
-
 app.use(require('./headers'))
+
+
 
 
     // Routing
@@ -21,24 +21,12 @@ router.use( (req, res, next) => {
 
 app.use('/api', router)      // all of our routes will be prefixed with /api
 router.route('/zipcode/:_id') 
-    .get((req, res) => {
-        console.log("The zipcode to search: ", req.params._id)
 
-
-        let zipCode = req.params._id
-        let temp1 = {}
-        axios.get("http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&APPID=a90a1769668d41f38e80d8ee118e7760")
-            .then((response) => { temp1 = response.data  })
-            
-        setTimeout(() => {
-            console.log("Data received from the WEATHER-API:")
-            console.log(typeof temp1)
-            console.log(" ")
-            console.log(temp1)
-
-            res.send( temp1 )
-        }, 250)
+    .get( async (req, res) => {
+        let temp1 = await axios.get("http://api.openweathermap.org/data/2.5/weather?zip=" + req.params._id + ",us&APPID=a90a1769668d41f38e80d8ee118e7760")
+        await res.send( temp1.data )
     })
+
 
 
 
@@ -50,26 +38,3 @@ app.listen(5000, (err) => {
     console.log(" \n Edmundo's WEATHER-API  UP & RUNNING...")
 })
 
-
-
-
-
-
-
-
-
-
-
-    // Old code I use when making the call to the Weather API with hard code data
-// ================================================================================
-// let zipCode = '85732'
-// let temp1 = 'mp'
-// axios.get("http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&APPID=a90a1769668d41f38e80d8ee118e7760")
-//     .then((response) => { temp1 = response.data  })
-    
-// setTimeout(() => {
-//     console.log("Data received from the WEATHER-API:")
-//     console.log(typeof temp1)
-//     console.log(" ")
-//     console.log(temp1)
-// }, 500)
