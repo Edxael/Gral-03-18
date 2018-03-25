12ab585a028e2527169f8453e9bcee5643b77aaa
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-24 21:36:42 -0600

    update Node.js AWS code

--- 05-AWS/01-Node-temp-1/handler.js
+++ 05-AWS/01-Node-temp-1/handler.js
@@ -1,11 +1,14 @@
 'use strict';
 
 module.exports.hello = (event, context, callback) => {
+
+  console.log(event.pathParameters.name)
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      
+      message:  'Hello ' + event.pathParameters.name + ' from here....' 
     }),
   };
 
@@ -14,3 +17,8 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+
+
+
+// message: 'Go Serverless v1.0! Your function executed successfully!',
